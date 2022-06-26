f07d8f7dc83b17be3eeea44d004fe0013ff8de0d
gabriel <gvargas@pickit.net>
2022-06-26 12:52:54 -0300

    typeorm and mysql database added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './Controllers/app.controller';
 import { MovieController } from './Controllers/MovieController';
 import { MovieDao } from './Daos/MovieDao';
@@ -6,7 +7,20 @@ import { AppService } from './Services/app.service';
 import { MovieService } from './Services/MovieService';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'liteflix-mysql',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'liteflix_db',
+    entities: ["apps/src/Models/Entities/**/*.{ts,js}"],
+    migrations: ["apps/src/Migrations/**/*.{ts,js}"],
+    synchronize: false,
+    autoLoadEntities: false,
+    keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController, MovieController],
   providers: [AppService, MovieService, MovieDao],
 })
